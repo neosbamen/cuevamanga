aae516031668d889be898c5a4bb12024e6a091b7
NothJosue <thedarkm04@gmail.com>
2025-09-30 10:44:28 -0500

    first commit

---
+++ src/main/java/com/cuevamangapp/cuevamanga/CuevamangaApplication.java
@@ -0,0 +1,13 @@
+package com.cuevamangapp.cuevamanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuevamangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuevamangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuevamangapp/cuevamanga/CuevamangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuevamangapp.cuevamanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuevamangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
